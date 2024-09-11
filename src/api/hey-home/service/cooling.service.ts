69f6b326f870acb1a811db9a3989eed811ecfe10
sungmun <tjdans174@mealant.com>
2024-09-11 21:24:24 +0900

    fix: cooling 서비스에 중복이 되는 부분 제거후 로깅 다시 추가

--- src/api/hey-home/service/cooling.service.ts
+++ src/api/hey-home/service/cooling.service.ts
@@ -1,52 +1,41 @@
 import deviceDao from './../../../dao/sqlite/device.dao';
 // import deviceLogDao from './../../../dao/sqlite/device-log.dao';
 import liveDeviceDao from './../../../dao/local/live-device.dao';
-import { Sensor } from './../../../types/device.type';
+import { Device, Sensor } from './../../../types/device.type';
 import heyHomeAgent from './../../../components/hey-home-request';
 import _ from 'lodash';
 import { Room } from '../../../types/room.type';
-import logger, { DeviceLogger } from '../../../config/logger';
+import logger from '../../../config/logger';
 import { EventEmitter } from 'stream';
+const airConditionerChange = async (airConditioners: Device[], deviceStatus, eventEmitter?: EventEmitter) => {
+  const power = {
+    켜짐: 'true',
+    꺼짐: 'false',
+  };
+  const filterAirConditioners = airConditioners.filter(airConditioner => {
+    const id = airConditioner.id;
+    const status = liveDeviceDao.liveDevices.get(id);
+    if (status) {
+      return status.power !== deviceStatus.power;
+    }
+    return true;
+  });
 
-const airConditionerOn = async (airConditioners, eventEmitter?: EventEmitter) => {
-  await airConditioners
-    .filter(airConditioner => {
-      const id = airConditioner.id;
-      const status = liveDeviceDao.liveDevices.get(id);
+  if (filterAirConditioners.length === 0) return;
 
-      if (status) {
-        return status.power !== '켜짐';
-      }
-      return true;
-    })
-    .reduce(async (promise, airConditioner) => {
-      const deviceStatus = { power: 'true', temperature: 18, fanSpeed: 3, mode: 0 };
-      promise
-        .then(async () =>
-          heyHomeAgent.post(`/openapi/control/${airConditioner.id}`, {
-            requirments: deviceStatus,
-          }),
-        )
-        .then(async () => airconStatusChange(airConditioner.id, eventEmitter));
-    }, Promise.resolve());
-};
-const airConditionerOff = async (airConditioners, eventEmitter?: EventEmitter) => {
-  await airConditioners
-    .filter(airConditioner => {
-      const id = airConditioner.id;
-      const status = liveDeviceDao.liveDevices.get(id);
-      return status?.power !== '꺼짐';
-    })
-    .reduce(async (promise, airConditioner) => {
-      const deviceStatus = { power: '꺼짐', temperature: 17, fanSpeed: 3, mode: 0 };
-      promise
-        .then(async () =>
-          heyHomeAgent.post(`/openapi/control/${airConditioner.id}`, {
-            requirments: deviceStatus,
-          }),
-        )
-        .then(async () => airconStatusChange(airConditioner.id, eventEmitter));
-    }, Promise.resolve());
+  logger.info(`aircon ${deviceStatus.power}`);
+  await filterAirConditioners.reduce(async (promise, airConditioner) => {
+    promise
+      .then(async () =>
+        heyHomeAgent.post(`/openapi/control/${airConditioner.id}`, {
+          requirments: {
+            ...deviceStatus,
+            power: power[deviceStatus.power],
+          },
+        }),
+      )
+      .then(async () => airconStatusChange(airConditioner.id, eventEmitter));
+  }, Promise.resolve());
 };
 
 const airconStatusChange = async (airconId: string, eventEmitter?: EventEmitter) => {
@@ -59,7 +48,13 @@ const airconStatusChange = async (airconId: string, eventEmitter?: EventEmitter)
 
 const exec = async (sensor: Sensor, room: Room, eventEmitter?: EventEmitter) => {
   const airConditioners = await deviceDao.findDeviceByDeviceType('IrAirconditioner');
-  if (sensor.temperature <= room.maxTemperature && sensor.temperature >= room.minTemperature) {
+
+  let power = 'true';
+  if (sensor.temperature > room.maxTemperature) {
+    power = '켜짐';
+  } else if (sensor.temperature < room.minTemperature) {
+    power = '꺼짐';
+  } else {
     logger.info(`cooling process 중지(${room.name})`, {
       data: {
         max: sensor.temperature <= room.maxTemperature,
@@ -71,11 +66,9 @@ const exec = async (sensor: Sensor, room: Room, eventEmitter?: EventEmitter) => 
     });
     return;
   }
-  if (sensor.temperature > room.maxTemperature) {
-    await airConditionerOn(airConditioners, eventEmitter);
-  } else if (sensor.temperature < room.minTemperature) {
-    await airConditionerOff(airConditioners, eventEmitter);
-  }
+
+  const deviceStatus = { power, temperature: 18, fanSpeed: 3, mode: 0 };
+  await airConditionerChange(airConditioners, deviceStatus, eventEmitter);
 };
 
 export default { exec };
--- src/watch.ts
+++ src/watch.ts
@@ -1,5 +1,5 @@
 import coolingService from './api/hey-home/service/cooling.service';
-import EventEmitter, { getEventListeners } from 'events';
+import EventEmitter from 'events';
 import roomDao from './dao/sqlite/room.dao';
 import logger, { DeviceLogger } from './config/logger';
 import deviceDao from './dao/sqlite/device.dao';
@@ -13,25 +13,22 @@ export default async (eventEmitter: EventEmitter) => {
       return roomDao
         .findOneRoomBySensorId(room.sensorId)
         .then(eventRoom => {
-          logger.info(`cooling process start(${eventRoom.name})`);
           if ([null, 0].includes(eventRoom.active)) return;
-          // logger.info(`cooling processing...`, { data: { data, eventRoom } });
+          logger.info(`cooling processing(${eventRoom.name})...`, { data: { sensor: data, room: eventRoom } });
           return coolingService.exec({ ...data, sensorId: room.sensorId }, eventRoom, eventEmitter);
         })
         .catch(error => {
           logger.error('cooling processing error', { data: error });
-        })
-        .finally(() => {
-          logger.info(`cooling process end`);
         });
     });
   });
+
   const aircons = await deviceDao.findDeviceByDeviceType('IrAirconditioner');
 
   aircons.forEach(aircon => {
     let changeTime = { power: null, changeTime: null };
     eventEmitter.on(aircon.id, data => {
-      logger.info('aircon event listener', { data: { changeTime, data } });
+      // logger.info('aircon event listener', { data: { changeTime, data } });
       if (changeTime.power === data.power) return;
       if (changeTime.power != null) {
         DeviceLogger.silly(
@@ -39,21 +36,17 @@ export default async (eventEmitter: EventEmitter) => {
             (Date.now() - changeTime.changeTime) / 1000 / 60,
           )}분`,
         );
-        axios
-          .post(
-            environment.webhook.url,
-            `에어컨(${aircon.name})이 ${data.power} 으로 변경되는데 ${Math.floor(
-              (Date.now() - changeTime.changeTime) / 1000 / 60,
-            )}분 걸렸습니다`,
-            {
-              headers: {
-                Title: `aircon ${data.power === '켜짐' ? 'on' : 'off'}`,
-              },
+        axios.post(
+          environment.webhook.url,
+          `에어컨(${aircon.name})이 ${data.power} 으로 변경되는데 ${Math.floor(
+            (Date.now() - changeTime.changeTime) / 1000 / 60,
+          )}분 걸렸습니다`,
+          {
+            headers: {
+              Title: `aircon ${data.power === '켜짐' ? 'on' : 'off'}`,
             },
-          )
-          .then(() => {
-            logger.info('webhook message send success');
-          });
+          },
+        );
       }
       changeTime.power = data.power;
       changeTime.changeTime = Date.now();
