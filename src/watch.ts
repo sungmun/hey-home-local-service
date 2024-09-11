40bd168460d516c701282a3b04a9708ad304d0da
sungmun <tjdans174@mealant.com>
2024-09-11 10:06:22 +0900

    fix: 에어컨 변경사항 발생시 로깅 가능하도록 수정

--- src/watch.ts
+++ src/watch.ts
@@ -1,5 +1,5 @@
 import coolingService from './api/hey-home/service/cooling.service';
-import EventEmitter from 'events';
+import EventEmitter, { getEventListeners } from 'events';
 import roomDao from './dao/sqlite/room.dao';
 import logger, { DeviceLogger } from './config/logger';
 import deviceDao from './dao/sqlite/device.dao';
@@ -31,24 +31,29 @@ export default async (eventEmitter: EventEmitter) => {
   aircons.forEach(aircon => {
     let changeTime = { power: null, changeTime: null };
     eventEmitter.on(aircon.id, data => {
-      if (changeTime?.power === data.power) return;
-      if (changeTime?.power != null) {
+      logger.info('aircon event listener', { data: { changeTime, data } });
+      if (changeTime.power === data.power) return;
+      if (changeTime.power != null) {
         DeviceLogger.silly(
           `change aircon status delay (${changeTime?.power}): ${Math.floor(
             (Date.now() - changeTime.changeTime) / 1000 / 60,
           )}분`,
         );
-        axios.post(
-          environment.webhook.url,
-          `에어컨(${aircon.name})이 ${changeTime.power} 으로 변경되는데 ${Math.floor(
-            (Date.now() - changeTime.changeTime) / 1000 / 60,
-          )}분 걸렸습니다`,
-          {
-            headers: {
-              title: `에어컨 ${changeTime.power}`,
+        axios
+          .post(
+            environment.webhook.url,
+            `에어컨(${aircon.name})이 ${changeTime.power} 으로 변경되는데 ${Math.floor(
+              (Date.now() - changeTime.changeTime) / 1000 / 60,
+            )}분 걸렸습니다`,
+            {
+              headers: {
+                title: `에어컨 ${changeTime.power}`,
+              },
             },
-          },
-        );
+          )
+          .then(() => {
+            logger.info('webhook message send success');
+          });
       }
       changeTime.power = data.power;
       changeTime.changeTime = Date.now();
