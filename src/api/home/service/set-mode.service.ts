2555ce468de5ea5df2afe79181904eef0297bcc1
sungmun <tjdans174@mealant.com>
2024-09-12 12:00:35 +0900

    feat: 온도관련데이터는 소수점으로 전달되도록 수정

--- src/api/home/service/set-mode.service.ts
+++ src/api/home/service/set-mode.service.ts
@@ -2,8 +2,13 @@ import roomDao from '../../../dao/sqlite/room.dao';
 
 const exec = async (mode: string) => {
   await roomDao.updateRoomActiveByName(mode);
-  const room = roomDao.findOneRoomByName(mode);
-  return room;
+  const room = await roomDao.findOneRoomByName(mode);
+  return {
+    ...room,
+    minTemperature: room.minTemperature / 100,
+    maxTemperature: room.maxTemperature / 100,
+    temperature: room.temperature / 100,
+  };
 };
 
 export default { exec };
